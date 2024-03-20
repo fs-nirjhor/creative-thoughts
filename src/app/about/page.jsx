34fbd48daeec610b1ca948745c762394fc5fb00c
fs-nirjhor <fsnirjhor2002@gmail.com>
2024-03-20 16:44:52 +0600

    about image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,16 @@
+import Image from "next/image";
+import about from "@/../public/about.png";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <section className="flex justify-between items-center gap-2">
+      <div>
+        <h1>Creative Thoughts</h1>
+      </div>
+      <div className="w-1/2">
+        <Image src={about} alt="about" />
+      </div>
+    </section>
+  );
 };
 export default AboutPage;
