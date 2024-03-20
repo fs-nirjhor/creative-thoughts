62bcc40f28a94ab75f6a5795917bbc8eaa731ec8
fs-nirjhor <fsnirjhor2002@gmail.com>
2024-03-20 20:20:51 +0600

    about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,7 +5,28 @@ const AboutPage = () => {
   return (
     <section className="flex justify-between items-center gap-3">
       <div>
-        <h1>Creative Thoughts</h1>
+        <h3 className="font-bold text-blue-700">About Company</h3>
+        <h1 className="text-6xl font-bold my-5">Creative Thoughts Agency.</h1>
+        <p className="mb-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          architecto in quas nihil et labore cupiditate voluptatem, quaerat
+          distinctio consequuntur ea similique omnis exercitationem delectus
+          fugit cumque laborum vero ex!
+        </p>
+        <div className="flex justify-between">
+          <div>
+            <h4 className="font-bold text-blue-700 text-lg">10 K+</h4>
+            <p className="text-sm">Year of experience</p>
+          </div>
+          <div>
+            <h4 className="font-bold text-blue-700 text-lg">274 K+</h4>
+            <p className="text-sm">People reached</p>
+          </div>
+          <div>
+            <h4 className="font-bold text-blue-700 text-lg">5 K+</h4>
+            <p className="text-sm">Services and plugins</p>
+          </div>
+        </div>
       </div>
       <Image
         src={about}
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="flex justify-between items-center text-sm text-gray-500 pb-3 mt-5">
-      <p>FS-Nirjhor</p>
+      <p className="font-bold">FS-Nirjhor</p>
       <p>FSN creative thoughts agency @ copyright reserved</p>
     </footer>
   );
