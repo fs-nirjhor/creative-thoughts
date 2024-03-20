6aa1f05e2d6e93b782a0348d369e192e8032955b
fs-nirjhor <fsnirjhor2002@gmail.com>
2024-03-20 16:09:22 +0600

    menu, image

--- src/components/navbar/NavLinks.jsx
+++ src/components/navbar/NavLinks.jsx
@@ -1,8 +1,10 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import menu from "@/../public/menu.png";
 
 const NavLinks = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -54,10 +56,10 @@ const NavLinks = () => {
       {/* mobile display nav */}
       <div className="absolute top-3 right-0 flex xl:hidden flex-col justify-center gap-3">
         <button
-          className="rounded-md px-2 py-1 bg-foreground text-background"
+          className="rounded-md px-2 py-1"
           onClick={() => setIsOpen(!isOpen)}
         >
-          Menu
+          <Image src={menu} alt="menu" className="h-5 w-6 ms-auto" />
         </button>
         {isOpen && (
           <nav className="min-h-[calc(100vh-4rem)] py-5 px-20 flex flex-col items-center gap-3 bg-secondary-background rounded">
@@ -68,6 +70,7 @@ const NavLinks = () => {
                 className={`rounded-full px-2 py-1 ${
                   link.path == pathname && "bg-foreground text-background"
                 }`}
+                onClick={() => setIsOpen(!isOpen)}
               >
                 {link.title}
               </Link>
@@ -76,6 +79,7 @@ const NavLinks = () => {
               <Link
                 href="/logout"
                 className="rounded-md px-2 py-1 bg-foreground text-background"
+                onClick={() => setIsOpen(!isOpen)}
               >
                 Logout
               </Link>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import NavLinks from "@/components/navbar/NavLinks";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <header className="relative flex justify-between gap-5 py-3">
-      <div className="text-xl font-bold">FSN</div>
+      <Link href="/" className="text-xl font-bold">
+        FSN
+      </Link>
       <NavLinks />
     </header>
   );
