650e29105413d57c92e6adc71057ceac81209787
fs-nirjhor <fsnirjhor2002@gmail.com>
2024-03-20 20:09:06 +0600

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex justify-between items-center text-sm text-gray-500 pb-3 mt-5">
+      <p>FS-Nirjhor</p>
+      <p>FSN creative thoughts agency @ copyright reserved</p>
+    </footer>
+  );
 };
 export default Footer;
